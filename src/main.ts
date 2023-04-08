38d0966bd7e0eb4019ec6ada348b30da09f9a28a
Jesus Marcano <jsusmarcano.jm@gmail.com>
2023-04-08 14:18:06 -0400

    remove baseUrl, add body-parser and loggin middleware

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import express from 'express'
+import morgan from 'morgan'
+import bodyParser from 'body-parser'
 import { PORT } from './config'
 
 const app = express()
+
+// middlewares
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
 app.listen(PORT, () => {
   console.log(`Listen on port: ${PORT}`)
 })
